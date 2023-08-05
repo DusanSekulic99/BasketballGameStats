c82e520c58554ac07a6e4e582ef8a0449f23acd0
DusanSekulic9 <dukiseki@gmail.com>
2023-08-05 23:16:19 +0200

    Init commit

---
+++ src/main/java/rs/raf/edu/rs/basketballgamestats/BasketballGameStatsApplication.java
@@ -0,0 +1,13 @@
+package rs.raf.edu.rs.basketballgamestats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballGameStatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballGameStatsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/rs/raf/edu/rs/basketballgamestats/BasketballGameStatsApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.raf.edu.rs.basketballgamestats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasketballGameStatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
